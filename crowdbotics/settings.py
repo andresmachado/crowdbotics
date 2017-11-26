b8a4f69dc7a7cf17b3bd3a52dc480417205a5f6c
André Machado <csantos.machado@gmail.com>
2017-11-25 23:30:37 -0200

    Added static_root setting and dj_database

--- crowdbotics/settings.py
+++ crowdbotics/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,12 +77,7 @@ WSGI_APPLICATION = 'crowdbotics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crowdbotics',
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
@@ -131,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/login/'
 LOGOUT_REDIRECT_URL = '/'
